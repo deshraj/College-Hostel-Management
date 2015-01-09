85a5ba72a1570a4e2d94dd1120bbc6df45fe5c76
deshraj <deshrajdry@gmail.com>
2015-01-10 01:46:34 +0530

    settings configured

--- hostelMgmt/hostelMgmt/settings.py
+++ hostelMgmt/hostelMgmt/settings.py
@@ -80,3 +80,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), \
+        'pyUrl/templates')
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
